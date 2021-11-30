2ecf2bd105d6030f3460d3871a1089485d867563
Juan Manuel Amavet <jmamavet@gmail.com>
2021-11-30 01:23:25 -0300

    cambios

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 // Defino clases
-
 class Ticket {
     constructor() {
         this.fechaRecital = null;
@@ -14,32 +13,29 @@ class Ticket {
         this.ubicacion = ubicacion;
     }
 
-    precioTicket() {
-        let precioTicket = this.ubicacion.precio;
-        return precioTicket;
-    }
-
     verFechaRecital() {
-        let fechaRecital = this.fechaRecital.fecha;
-        return fechaRecital;
+        return this.fechaRecital.fecha;
     }
 
     verUbicacion() {
-        let ubicacionRecital = this.ubicacion.nombre;
-        return ubicacionRecital;
+        return this.ubicacion.nombre;
     }
 
     verPrecioUbicacion() {
-        let precioUbicacion = this.ubicacion.precio;
-        return precioUbicacion;
+        return this.ubicacion.precio;
     }
 }
 
 class Compra {
     constructor() {
+        this.nombreCliente = null;
         this.ticket = null;
         this.estacionamiento = null;
         this.fechaEntrega = null;
+    }
+
+    definirNombreCliente(nombreCliente) {
+        this.nombreCliente = nombreCliente;
     }
 
     agregarTicket(ticket) {
@@ -54,24 +50,24 @@ class Compra {
         this.fechaEntrega = fecha;
     }
 
-    precioFinal() {
-        let precioFinal = this.estacionamiento.precio + this.ticket.precioTicket();
-        return precioFinal;
+    verNombreCliente() {
+        return this.nombreCliente;
     }
 
     verEstacionamiento() {
-        let nombreEstacionamiento = this.estacionamiento.nombre;
-        return nombreEstacionamiento;
+        return this.estacionamiento.nombre;
     }
 
     verPrecioEstacionamiento() {
-        let precioEstacionamiento = this.estacionamiento.precio;
-        return precioEstacionamiento;
+        return this.estacionamiento.precio;
     }
 
     verFechaEntrega() {
-        let fechaDeEntrega = this.fechaEntrega.fecha;
-        return fechaDeEntrega;
+        return this.fechaEntrega.fecha;
+    }
+
+    precioFinal() {
+        return this.estacionamiento.precio + this.ticket.verPrecioUbicacion();
     }
 
 }
@@ -86,68 +82,122 @@ const estacionamientos = [{ id: 1, nombre: 'Descubierto', precio: 300 }, { id: 2
 const fechasEntrega = [{ id: 1, fecha: '05/01/2022' }, { id: 2, fecha: '08/01/2022' }, { id: 3, fecha: '11/01/2022' }];
 
 
+// Genero en el DOM todas las opciones para el ticket tomando los valores de los objetos literales
+let opcionesFechaRecital = document.getElementById("opcionesFechaRecital");
+fechasRecital.forEach(element => {
+    let opcionFechaRecital = document.createElement("option");
+    opcionFechaRecital.setAttribute("value", element.id);
+    opcionFechaRecital.innerHTML = element.fecha;
+    opcionesFechaRecital.appendChild(opcionFechaRecital);
+});
 
-const cliente = prompt('Hola! Bienvenid@, por favor ingresa tu nombre');
+let opcionesUbicacion = document.getElementById("opcionesUbicacion");
+ubicaciones.forEach(element => {
+    let opcionUbicacion = document.createElement("option");
+    opcionUbicacion.setAttribute("value", element.id);
+    opcionUbicacion.innerHTML = element.nombre + " - $" + element.precio;
+    opcionesUbicacion.appendChild(opcionUbicacion);
+});
 
-const hacerCompra = confirm(`Hola ${cliente}, ¿querés realizar una compra?`);
+let opcionesEstacionamiento = document.getElementById("opcionesEstacionamiento");
+estacionamientos.forEach(element => {
+    let opcionEstacionamiento = document.createElement("option");
+    opcionEstacionamiento.setAttribute("value", element.id);
+    opcionEstacionamiento.innerHTML = element.nombre + " - $" + element.precio;
+    opcionesEstacionamiento.appendChild(opcionEstacionamiento);
+});
 
-if (hacerCompra) {
+let opcionesFechaRetiro = document.getElementById("opcionesFechaRetiro");
+fechasEntrega.forEach(element => {
+    let opcionFechaRetiro = document.createElement("option");
+    opcionFechaRetiro.setAttribute("value", element.id);
+    opcionFechaRetiro.innerHTML = element.fecha;
+    opcionesFechaRetiro.appendChild(opcionFechaRetiro);
+});
 
-    const nuevaCompra = new Compra();
-    const nuevoTicket = new Ticket();
+// Creo objetos para nuevo ticket y nueva compra
+const nuevaCompra = new Compra();
+const nuevoTicket = new Ticket();
 
-    // Se ofrecen las distintas fechas al usuario
-    let fechaRecitalID = parseInt(prompt(`Elegi la fecha del recital: ${fechasRecital.map((fecha) => `${fecha.id}) ${fecha.fecha}`).join(' ')}`),);
-    let fechaRecital = fechasRecital.find((fecha) => fecha.id === fechaRecitalID);
+// Valido formulario al clickear Confirmar en el form
+let formularioCompra = document.getElementById("formularioCompra");
+formularioCompra.addEventListener("submit", validarFormulario);
 
-    while (!fechaRecital) {
-        fechaRecitalID = parseInt(prompt(`Valor incorrecto, por favor elegi la fecha del recital: ${fechasRecital.map((fecha) => `${fecha.id}) ${fecha.fecha}`).join(' ')}`),);
-        fechaRecital = fechasRecital.find((fecha) => fecha.id === fechaRecitalID);
+// Funcion para validar el formulario y pasarle los valores validados a los objetos
+function validarFormulario(e) {
+    let nombreUsuario = document.getElementById("nombreUsuario");
+
+    if (nombreUsuario.value == "") {
+        e.preventDefault();
+        let errorNombreVacio = document.getElementById("errorNombreVacio");
+        errorNombreVacio.innerHTML = "Por favor ingresa tu nombre para poder continuar";
+    } else {
+        nuevaCompra.definirNombreCliente(nombreUsuario.value);
     }
 
-    nuevoTicket.definirFecha(fechaRecital);
-
-    // Se ofrecen las distintas ubicaciones al usuario
-    let ubicacionRecitalID = parseInt(prompt(`Elegi la ubicacion: ${ubicaciones.map((ubicacion) => `${ubicacion.id}) ${ubicacion.nombre} - $${ubicacion.precio}`).join(' ')}`),);
-    let ubicacionRecital = ubicaciones.find((ubicacion) => ubicacion.id === ubicacionRecitalID);
-
-    while (!ubicacionRecital) {
-        ubicacionRecitalID = parseInt(prompt(`Valor incorrecto, por favor elegi la ubicacion: ${ubicaciones.map((ubicacion) => `${ubicacion.id}) ${ubicacion.nombre} - $${ubicacion.precio}`).join(' ')}`),);
-        ubicacionRecital = ubicaciones.find((ubicacion) => ubicacion.id === ubicacionRecitalID);
+    if (opcionesFechaRecital.value == "0") {
+        e.preventDefault();
+        let errorFechaRecitalVacia = document.getElementById("errorFechaRecitalVacia");
+        errorFechaRecitalVacia.innerHTML = "Por favor selecciona una fecha para el recital";
+    } else {
+        fechasRecital.forEach(element => {
+            if (element.id == opcionesFechaRecital.value) {
+                nuevoTicket.definirFecha(element);
+            }
+        });
     }
 
-    nuevoTicket.definirUbicacion(ubicacionRecital);
-
-    // Se ofrecen las distintas opciones de estacionamiento al usuario
-    let estacionamientoID = parseInt(prompt(`Elegí tu estacionamiento: ${estacionamientos.map((estacionamiento) => `${estacionamiento.id}) ${estacionamiento.nombre} - $${estacionamiento.precio} `).join(' ')}`),);
-    let estacionamiento = estacionamientos.find((estacionamiento) => estacionamiento.id === estacionamientoID)
-
-    while (!estacionamiento) {
-        estacionamientoID = parseInt(prompt(`Valor incorrecto, por favor elegí tu estacionamiento: ${estacionamientos.map((estacionamiento) => `${estacionamiento.id}) ${estacionamiento.nombre} - $${estacionamiento.precio} `).join(' ')}`),);
-        estacionamiento = estacionamientos.find((estacionamiento) => estacionamiento.id === estacionamientoID);
+    if (opcionesUbicacion.value == "0") {
+        e.preventDefault();
+        let errorUbicacionVacia = document.getElementById("errorUbicacionVacia");
+        errorUbicacionVacia.innerHTML = "Por favor selecciona una ubicacion";
+    } else {
+        ubicaciones.forEach(element => {
+            if (element.id == opcionesUbicacion.value) {
+                nuevoTicket.definirUbicacion(element);
+            }
+        });
     }
 
-    let fechaEntregaID = parseInt(prompt(`Elegi la fecha de entrega: ${fechasEntrega.map((fecha) => `${fecha.id}) ${fecha.fecha}`).join(' ')}`),);
-    let fechaEntrega = fechasEntrega.find((fecha) => fecha.id === fechaEntregaID);
-
-    while (!fechaEntrega) {
-        fechaEntregaID = parseInt(prompt(`Valor incorrecto, por favor elegi la fecha del recital: ${fechasEntrega.map((fecha) => `${fecha.id}) ${fecha.fecha}`).join(' ')}`),);
-        fechaEntrega = fechasEntrega.find((fecha) => fecha.id === fechaEntregaID);
+    if (opcionesFechaRetiro.value == "0") {
+        e.preventDefault();
+        let errorFechaRetiroVacia = document.getElementById("errorFechaRetiroVacia");
+        errorFechaRetiroVacia.innerHTML = "Por favor selecciona una fecha para retirar tu entrada";
+    } else {
+        fechasEntrega.forEach(element => {
+            if (element.id == opcionesFechaRetiro.value) {
+                nuevaCompra.definirFechaEntrega(element);
+            }
+        });
     }
 
-    // Se agrega el ticket a la compra y se define estacionamiento y fecha de entrega
+    if (opcionesEstacionamiento.value == "0") {
+        nuevaCompra.definirEstacionamiento("Sin estacionamiento");
+    } else {
+        estacionamientos.forEach(element => {
+            if (element.id == opcionesEstacionamiento.value) {
+                nuevaCompra.definirEstacionamiento(element);
+            }
+        });
+    }
+
     nuevaCompra.agregarTicket(nuevoTicket);
-    nuevaCompra.definirEstacionamiento(estacionamiento);
-    nuevaCompra.definirFechaEntrega(fechaEntrega);
 
+    let nombreDeCliente = nuevaCompra.verNombreCliente();
     let fechaDeRecital = nuevoTicket.verFechaRecital();
-    let ubicacionDeRecital = nuevoTicket.verUbicacion();
+    let ubicacionElegida = nuevoTicket.verUbicacion();
     let precioUbicacion = nuevoTicket.verPrecioUbicacion();
-    let nombreEstacionamiento = nuevaCompra.verEstacionamiento();
+    let estacionamientoElegido = nuevaCompra.verEstacionamiento();
     let precioEstacionamiento = nuevaCompra.verPrecioEstacionamiento();
     let fechaDeEntrega = nuevaCompra.verFechaEntrega();
     let totalCompra = nuevaCompra.precioFinal();
 
+    // Llamo a la funcion que genera la tabla dinamica en el DOM con los valores seleccionados
+    generarTabla(nombreDeCliente, fechaDeRecital, ubicacionElegida, precioUbicacion, estacionamientoElegido, precioEstacionamiento, fechaDeEntrega, totalCompra);
+}
+
+// Funcion para generar dinamicamente la tabla en el DOM
+function generarTabla(nombreDeCliente, fechaDeRecital, ubicacionElegida, precioUbicacion, estacionamientoElegido, precioEstacionamiento, fechaDeEntrega, totalCompra) {
     // Genero tabla en el DOM con el resumen de compra
     let tabla = document.createElement("table");
     tabla.setAttribute("class", "table table-striped table-light");
@@ -155,6 +205,8 @@ if (hacerCompra) {
     let tablaHead = document.createElement("thead");
     tablaHead.setAttribute("class", "bg-light");
 
+    let header0 = document.createElement("th");
+    header0.innerHTML = "Nombre Del Cliente";
     let header1 = document.createElement("th");
     header1.innerHTML = "Fecha Del Recital";
     let header2 = document.createElement("th");
@@ -170,6 +222,7 @@ if (hacerCompra) {
     let header7 = document.createElement("th");
     header7.innerHTML = "Total Compra";
 
+    tablaHead.appendChild(header0);
     tablaHead.appendChild(header1);
     tablaHead.appendChild(header2);
     tablaHead.appendChild(header3);
@@ -182,18 +235,16 @@ if (hacerCompra) {
 
     let tablaBody = document.createElement("tbody");
     let fila = document.createElement("tr");
-    fila.innerHTML = `<td>${fechaDeRecital}</td>
-                      <td>${ubicacionDeRecital}</td>
-                      <td>${precioUbicacion}</td>
-                      <td>${nombreEstacionamiento}</td>
-                      <td>${precioEstacionamiento}</td>
-                      <td>${fechaDeEntrega}</td>
-                      <td>${totalCompra}</td>`;
+    fila.innerHTML = `  <td>${nombreDeCliente}</td>
+                            <td>${fechaDeRecital}</td>
+                            <td>${ubicacionElegida}</td>
+                            <td>${precioUbicacion}</td>
+                            <td>${estacionamientoElegido}</td>
+                            <td>${precioEstacionamiento}</td>
+                            <td>${fechaDeEntrega}</td>
+                            <td>${totalCompra}</td>`;
     tablaBody.appendChild(fila);
 
     tabla.appendChild(tablaBody);
     document.getElementById("compra").appendChild(tabla);
-
-} else {
-    alert('Te esperamos la proxima!');
 }
